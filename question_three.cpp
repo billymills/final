fa74e179a5c33d5b910549ab333cbdfb0d6cc840
Billy Mills <wmills@unca.edu>
2012-12-04 11:59:10 -0500

    file seems to be opening

--- question_three.cpp
+++ question_three.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <cstdlib>
+#include <fstream>
 
 using std::cout;
 using std::endl;
+using std::ifstream;
 
 int main (int argc, char** argv){
 	
@@ -13,6 +15,12 @@ int main (int argc, char** argv){
 	else {
 		cout << "filename: " << argv[1] << endl;  //name of file
 		cout << "threshold: " << argv[2] << endl; //threshold
+
+		ifstream file (argv[1]);
+		if (!file.is_open())
+			cout << "could not open file" << endl;
+		else
+			cout << "the file is open" << endl;
 	}	
 	return 0;
 
